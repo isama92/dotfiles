88181c97d58f68fa54c97559956f702f8d03fe05
Stefano Borzoni <isama92@gmail.com>
2026-05-03 19:59:01 +0200

    starship init

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -6,6 +6,7 @@ local config = wezterm.config_builder()
 
 config.initial_cols = 128
 config.initial_rows = 28
+config.font = wezterm.font('FiraCode Nerd Font')
 config.font_size = 11
 config.color_scheme = 'Solarized Light (Gogh)'
 

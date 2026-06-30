54d1dac099eff5e0129798c8366ba7caae2e529f
Stefano Borzoni <borzoni@vcsw.nl>
2026-06-30 12:31:43 +0200

    moved nvm to .local/share/nvm

--- private_dot_config/shell/exports.sh
+++ private_dot_config/shell/exports.sh
@@ -17,7 +17,7 @@ else
 fi
 
 # NVM
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$HOME/.local/share/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
